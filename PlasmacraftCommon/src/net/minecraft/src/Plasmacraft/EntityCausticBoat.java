e91ee9226e38fd2bc01dcf2b65a2b3102a9e529d
Malkierian <Malkierian@gmail.com>
2011-12-16 23:25:35 -0700

    Fixed boat placement crash on client in smp mode, as well as the boat destruction drops (they now drop radionite ore when dropped).  Also changed the references to the worldgen stuff to the Plasmacraft package.

--- PlasmacraftCommon/src/net/minecraft/src/Plasmacraft/EntityCausticBoat.java
+++ PlasmacraftCommon/src/net/minecraft/src/Plasmacraft/EntityCausticBoat.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import net.minecraft.src.AxisAlignedBB;
 import net.minecraft.src.Block;
+import net.minecraft.src.DamageSource;
 import net.minecraft.src.Entity;
 import net.minecraft.src.EntityBoat;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Item;
 import net.minecraft.src.Material;
+import net.minecraft.src.MathHelper;
+import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.World;
 
-public class EntityCausticBoat extends EntityBoat
+public class EntityCausticBoat extends Entity
 {
     private int my_boatPosRotationIncrements;
     private double my_boatX;
@@ -25,14 +29,56 @@ public class EntityCausticBoat extends EntityBoat
     public EntityCausticBoat(World world)
     {
         super(world);
+        preventEntitySpawning = true;
+        setSize(1.5F, 0.6F);
+        yOffset = height / 2.0F;
+    }
+
+    protected boolean canTriggerWalking()
+    {
+        return false;
+    }
+
+    protected void entityInit()
+    {
+        dataWatcher.addObject(17, new Integer(0));
+        dataWatcher.addObject(18, new Integer(1));
+        dataWatcher.addObject(19, new Integer(0));
+    }
+
+    public AxisAlignedBB getCollisionBox(Entity entity)
+    {
+        return entity.boundingBox;
+    }
+
+    public AxisAlignedBB getBoundingBox()
+    {
+        return boundingBox;
+    }
+
+    public boolean canBePushed()
+    {
+        return true;
     }
 
     public EntityCausticBoat(World world, double d, double d1, double d2)
     {
-        super(world, d, d1, d2);
+        this(world);
+        setPosition(d, d1 + (double)yOffset, d2);
+        motionX = 0.0D;
+        motionY = 0.0D;
+        motionZ = 0.0D;
+        prevPosX = d;
+        prevPosY = d1;
+        prevPosZ = d2;
     }
 
-    public boolean attackEntityFrom(Entity entity, int i)
+    public double getMountedYOffset()
+    {
+        return (double)height * 0.0D - 0.30000001192092896D;
+    }
+
+    public boolean attackEntityFrom(DamageSource damagesource, int i)
     {
         if(PlasmaCraftCore.proxy.isMultiplayerWorld(worldObj) || isDead)
         {
@@ -44,6 +90,10 @@ public class EntityCausticBoat extends EntityBoat
         setBeenAttacked();
         if(getDamageTaken() > 40)
         {
+            if(riddenByEntity != null)
+            {
+                riddenByEntity.mountEntity(this);
+            }
             for(int j = 0; j < 2; j++)
             {
                 dropItemWithOffset(PlasmaCraftCore.ingotRadionite.shiftedIndex, 1, 0.0F);
@@ -52,6 +102,18 @@ public class EntityCausticBoat extends EntityBoat
             setEntityDead();
         }
         return true;
+    }
+
+    public void performHurtAnimation()
+    {
+        setForwardDirection(-getForwardDirection());
+        setTimeSinceHit(10);
+        setDamageTaken(getDamageTaken() * 11);
+    }
+
+    public boolean canBeCollidedWith()
+    {
+        return !isDead;
     }
 
     public void setPositionAndRotation2(double d, double d1, double d2, float f, 
@@ -77,7 +139,7 @@ public class EntityCausticBoat extends EntityBoat
 
     public void onUpdate()
     {
-        onEntityUpdate();
+        super.onUpdate();
         if(getTimeSinceHit() > 0)
         {
             setTimeSinceHit(getTimeSinceHit() - 1);
@@ -104,27 +166,50 @@ public class EntityCausticBoat extends EntityBoat
             }
         }
 
+        double d1 = Math.sqrt(motionX * motionX + motionZ * motionZ);
+        if(d1 > 0.14999999999999999D)
+        {
+            double d3 = Math.cos(((double)rotationYaw * 3.1415926535897931D) / 180D);
+            double d9 = Math.sin(((double)rotationYaw * 3.1415926535897931D) / 180D);
+            for(int i1 = 0; (double)i1 < 1.0D + d1 * 60D; i1++)
+            {
+                double d16 = rand.nextFloat() * 2.0F - 1.0F;
+                double d19 = (double)(rand.nextInt(2) * 2 - 1) * 0.69999999999999996D;
+                if(rand.nextBoolean())
+                {
+                    double d21 = (posX - d3 * d16 * 0.80000000000000004D) + d9 * d19;
+                    double d23 = posZ - d9 * d16 * 0.80000000000000004D - d3 * d19;
+                    worldObj.spawnParticle("splash", d21, posY - 0.125D, d23, motionX, motionY, motionZ);
+                } else
+                {
+                    double d22 = posX + d3 + d9 * d16 * 0.69999999999999996D;
+                    double d24 = (posZ + d9) - d3 * d16 * 0.69999999999999996D;
+                    worldObj.spawnParticle("splash", d22, posY - 0.125D, d24, motionX, motionY, motionZ);
+                }
+            }
+
+        }
         if(PlasmaCraftCore.proxy.isMultiplayerWorld(worldObj))
         {
             if(my_boatPosRotationIncrements > 0)
             {
-                double d1 = posX + (my_boatX - posX) / (double)my_boatPosRotationIncrements;
-                double d5 = posY + (my_boatY - posY) / (double)my_boatPosRotationIncrements;
-                double d9 = posZ + (my_boatZ - posZ) / (double)my_boatPosRotationIncrements;
-                double d12;
-                for(d12 = my_boatYaw - (double)rotationYaw; d12 < -180D; d12 += 360D) { }
-                for(; d12 >= 180D; d12 -= 360D) { }
-                rotationYaw += d12 / (double)my_boatPosRotationIncrements;
+                double d4 = posX + (my_boatX - posX) / (double)my_boatPosRotationIncrements;
+                double d10 = posY + (my_boatY - posY) / (double)my_boatPosRotationIncrements;
+                double d13 = posZ + (my_boatZ - posZ) / (double)my_boatPosRotationIncrements;
+                double d17;
+                for(d17 = my_boatYaw - (double)rotationYaw; d17 < -180D; d17 += 360D) { }
+                for(; d17 >= 180D; d17 -= 360D) { }
+                rotationYaw += d17 / (double)my_boatPosRotationIncrements;
                 rotationPitch += (my_boatPitch - (double)rotationPitch) / (double)my_boatPosRotationIncrements;
                 my_boatPosRotationIncrements--;
-                setPosition(d1, d5, d9);
+                setPosition(d4, d10, d13);
                 setRotation(rotationYaw, rotationPitch);
             } else
             {
-                double d2 = posX + motionX;
-                double d6 = posY + motionY;
-                double d10 = posZ + motionZ;
-                setPosition(d2, d6, d10);
+                double d5 = posX + motionX;
+                double d11 = posY + motionY;
+                double d14 = posZ + motionZ;
+                setPosition(d5, d11, d14);
                 if(onGround)
                 {
                     motionX *= 0.5D;
@@ -137,8 +222,18 @@ public class EntityCausticBoat extends EntityBoat
             }
             return;
         }
-        double d3 = d * 2D - 1.0D;
-        motionY += 0.039999999105930328D * d3;
+        if(d < 1.0D)
+        {
+            double d6 = d * 2D - 1.0D;
+            motionY += 0.039999999105930328D * d6;
+        } else
+        {
+            if(motionY < 0.0D)
+            {
+                motionY /= 2D;
+            }
+            motionY += 0.0070000002160668373D;
+        }
         if(riddenByEntity != null)
         {
             motionX += riddenByEntity.motionX * 0.20000000000000001D;
@@ -168,42 +263,14 @@ public class EntityCausticBoat extends EntityBoat
             motionZ *= 0.5D;
         }
         moveEntity(motionX, motionY, motionZ);
-        double d11 = Math.sqrt(motionX * motionX + motionZ * motionZ);
-        if(d11 > 0.14999999999999999D)
-        {
-            double d13 = Math.cos(((double)rotationYaw * 3.1415926535897931D) / 180D);
-            double d15 = Math.sin(((double)rotationYaw * 3.1415926535897931D) / 180D);
-            for(int i1 = 0; (double)i1 < 1.0D + d11 * 60D; i1++)
-            {
-                double d18 = rand.nextFloat() * 2.0F - 1.0F;
-                double d20 = (double)(rand.nextInt(2) * 2 - 1) * 0.69999999999999996D;
-                if(rand.nextBoolean())
-                {
-                    double d21 = (posX - d13 * d18 * 0.80000000000000004D) + d15 * d20;
-                    double d23 = posZ - d15 * d18 * 0.80000000000000004D - d13 * d20;
-                    worldObj.spawnParticle("splash", d21, posY - 0.125D, d23, motionX, motionY, motionZ);
-                } else
-                {
-                    double d22 = posX + d13 + d15 * d18 * 0.69999999999999996D;
-                    double d24 = (posZ + d15) - d13 * d18 * 0.69999999999999996D;
-                    worldObj.spawnParticle("splash", d22, posY - 0.125D, d24, motionX, motionY, motionZ);
-                }
-            }
-
-        }
-        if(isCollidedHorizontally && d11 > 0.14999999999999999D)
+        if(isCollidedHorizontally && d1 > 0.20000000000000001D)
         {
             if(!PlasmaCraftCore.proxy.isMultiplayerWorld(worldObj))
             {
                 setEntityDead();
-                for(int k = 0; k < 3; k++)
+                for(int j = 0; j < 2; j++)
                 {
-                    dropItemWithOffset(Block.planks.blockID, 1, 0.0F);
-                }
-
-                for(int l = 0; l < 2; l++)
-                {
-                    dropItemWithOffset(Item.stick.shiftedIndex, 1, 0.0F);
+                    dropItemWithOffset(PlasmaCraftCore.ingotRadionite.shiftedIndex, 1, 0.0F);
                 }
 
             }
@@ -247,9 +314,90 @@ public class EntityCausticBoat extends EntityBoat
             }
 
         }
+        for(int k1 = 0; k1 < 4; k1++)
+        {
+            int l1 = MathHelper.floor_double(posX + ((double)(k1 % 2) - 0.5D) * 0.80000000000000004D);
+            int i2 = MathHelper.floor_double(posY);
+            int j2 = MathHelper.floor_double(posZ + ((double)(k1 / 2) - 0.5D) * 0.80000000000000004D);
+            if(worldObj.getBlockId(l1, i2, j2) == Block.snow.blockID)
+            {
+                worldObj.setBlockWithNotify(l1, i2, j2, 0);
+            }
+        }
+        
         if(riddenByEntity != null && riddenByEntity.isDead)
         {
             riddenByEntity = null;
         }
+    }
+
+    public void updateRiderPosition()
+    {
+        if(riddenByEntity == null)
+        {
+            return;
+        } else
+        {
+            double d = Math.cos(((double)rotationYaw * 3.1415926535897931D) / 180D) * 0.40000000000000002D;
+            double d1 = Math.sin(((double)rotationYaw * 3.1415926535897931D) / 180D) * 0.40000000000000002D;
+            riddenByEntity.setPosition(posX + d, posY + getMountedYOffset() + riddenByEntity.getYOffset(), posZ + d1);
+            return;
+        }
+    }
+
+    protected void writeEntityToNBT(NBTTagCompound nbttagcompound)
+    {
+    }
+
+    protected void readEntityFromNBT(NBTTagCompound nbttagcompound)
+    {
+    }
+
+    public float getShadowSize()
+    {
+        return 0.0F;
+    }
+
+    public boolean interact(EntityPlayer entityplayer)
+    {
+        if(riddenByEntity != null && (riddenByEntity instanceof EntityPlayer) && riddenByEntity != entityplayer)
+        {
+            return true;
+        }
+        if(!PlasmaCraftCore.proxy.isMultiplayerWorld(worldObj))
+        {
+            entityplayer.mountEntity(this);
+        }
+        return true;
+    }
+
+    public void setDamageTaken(int i)
+    {
+        dataWatcher.updateObject(19, Integer.valueOf(i));
+    }
+
+    public int getDamageTaken()
+    {
+        return dataWatcher.getWatchableObjectInt(19);
+    }
+
+    public void setTimeSinceHit(int i)
+    {
+        dataWatcher.updateObject(17, Integer.valueOf(i));
+    }
+
+    public int getTimeSinceHit()
+    {
+        return dataWatcher.getWatchableObjectInt(17);
+    }
+
+    public void setForwardDirection(int i)
+    {
+        dataWatcher.updateObject(18, Integer.valueOf(i));
+    }
+
+    public int getForwardDirection()
+    {
+        return dataWatcher.getWatchableObjectInt(18);
     }
 }
